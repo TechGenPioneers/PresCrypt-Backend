b55ea776424fbb716bf7a908e6b29fd02abe469f
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-03-15 22:41:49 +0530

    add AdminDoctorController and DoctorDto

---
+++ PresCrypt-Backend/PresCrypt.API/Controllers/AdminDoctorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PresCrypt_Backend.PresCrypt.API.Dto;
+using System.Diagnostics;
+
+namespace PresCrypt_Backend.PresCrypt.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminDoctorController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<DoctorDto> AddDoctor([FromBody] DoctorDto newDoctorDto)
+        {
+            if (newDoctorDto == null)
+            {
+                return BadRequest("Doctor details are required.");
+            }
+
+            Debug.WriteLine(newDoctorDto);
+
+            return Ok(newDoctorDto);
+        }
+    }
+}
---
+++ PresCrypt-Backend/PresCrypt.API/Dto/DoctorDto.cs
@@ -0,0 +1,24 @@
+﻿namespace PresCrypt_Backend.PresCrypt.API.Dto
+{
+    public class DoctorDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public string specialization { get; set; }
+        public string slmcLicense { get; set; }
+        public string contactNumber { get; set; }
+        public string hospital { get; set; }
+
+        public override string ToString()
+        {
+            return $"Doctor: {FirstName}" + $" {LastName}, \n" +
+                   $"Specialization: {specialization}, \n" +
+                   $"SLMC License: {slmcLicense},\n" +
+                   $" Contact: {contactNumber}, \n" +
+                   $"Hospital: {hospital}, \n" +
+                   $"Email: {email}";
+        }
+
+    }
+}
