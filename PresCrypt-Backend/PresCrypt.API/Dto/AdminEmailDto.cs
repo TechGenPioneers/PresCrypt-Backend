16cdfa3eba5ef51cf0a960e42f561110121ed89e
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-04-11 18:28:01 +0530

    update send email

--- PresCrypt-Backend/PresCrypt.API/Dto/AdminEmailDto.cs
+++ PresCrypt-Backend/PresCrypt.API/Dto/AdminEmailDto.cs
@@ -3,6 +3,8 @@
     public class AdminEmailDto
     {
         public string Receptor { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Reason { get; set; }
     }
 }
