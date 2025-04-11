e002f9f28a85d24e79133669250a704c4a236ec3
Dewmin Deniyegedara <dewminkasmitha30@gmail.com>
2025-04-12 00:44:12 +0530

    fix:updated patient model

--- PresCrypt-Backend/PresCrypt.Core/Models/Patient.cs
+++ PresCrypt-Backend/PresCrypt.Core/Models/Patient.cs
@@ -22,7 +22,7 @@ namespace PresCrypt_Backend.PresCrypt.Core.Models
         public DateTime DOB { get; set; }
 
         [Required]
-        public char Gender { get; set; }
+        public string Gender { get; set; }
 
         [Required]
         [EmailAddress]
@@ -39,15 +39,11 @@ namespace PresCrypt_Backend.PresCrypt.Core.Models
 
         public DateTime UpdatedAt { get; set; }
 
-        [Required]
-        public string PasswordHash { get; set; }
-
         public string Status { get; set; }  // Example: "Active" or "Inactive"
 
         public DateTime? LastLogin { get; set; } // Nullable in case they haven't logged in
         [Required]
-        public string ContactNo { get; set; }//should be removed because have another tble
-      // Relationship with Appointments
+        
         public ICollection<Appointment> Appointments { get; set; }
     }
 }
