35ce51d84328fbd9dafcc02cc35bd8cdfb0a8f39
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-04-18 00:33:12 +0530

    fix the errors when merging

--- PresCrypt-Backend/PresCrypt.API/Controllers/DoctorController.cs
+++ PresCrypt-Backend/PresCrypt.API/Controllers/DoctorController.cs
@@ -83,7 +83,7 @@ namespace PresCrypt_Backend.PresCrypt.API.Controllers
         [HttpGet("book/{doctorId}")]//for this I used mapster
         public async Task<ActionResult<List<DoctorBookingDto>>> GetDoctorBookedbyId(string doctorId)
         {
-            var doctor = (await _context.Doctor.FindAsync(doctorId));
+            var doctor = await _context.Doctor.FindAsync(doctorId);
             if (doctor is null)
             {
                 return NotFound();
@@ -93,7 +93,7 @@ namespace PresCrypt_Backend.PresCrypt.API.Controllers
             return Ok(response);
 
         }
-        //test
+        
 
 
     }
