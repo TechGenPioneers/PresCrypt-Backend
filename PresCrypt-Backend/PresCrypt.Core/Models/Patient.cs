a77625ce671a9206a3758d1c607601fc92b25cfa
Dewmin Deniyegedara <dewminkasmitha30@gmail.com>
2025-05-08 11:28:31 +0530

    fix:updated patient.cs

--- PresCrypt-Backend/PresCrypt.Core/Models/Patient.cs
+++ PresCrypt-Backend/PresCrypt.Core/Models/Patient.cs
@@ -1,15 +1,13 @@
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 namespace PresCrypt_Backend.PresCrypt.Core.Models
 
 {
     public class Patient
     {
-        
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public required string PatientId { get; set; } = string.Empty; // Primary Key
 
@@ -18,7 +16,6 @@ namespace PresCrypt_Backend.PresCrypt.Core.Models
 
         [Required]
         public required string LastName { get; set; }
-
         [Required]
         public DateTime DOB { get; set; }
 
@@ -28,24 +25,37 @@ namespace PresCrypt_Backend.PresCrypt.Core.Models
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+        [ForeignKey("Email")]
+        public User User { get; set; }
 
-        public string BloodGroup { get; set; }
+        public string? BloodGroup { get; set; }
+
+        
+        public string? NIC { get; set; }
 
         [Required]
-        public string NIC { get; set; }
+        public string Address { get; set; }    
+         public byte[] ProfileImage { get; set; }
 
-        public byte[] ProfileImage { get; set; }
 
-        public DateTime CreatedAt { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+       
 
-        public string Status { get; set; }  // Example: "Active" or "Inactive"
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public string Status { get; set; }  
 
         public DateTime? LastLogin { get; set; } // Nullable in case they haven't logged in
         [Required]
-       
+        public string ContactNo { get; set; }
+
+        public string? ResetToken { get; set; }
+        public DateTime? ResetTokenExpiry { get; set; }
         public ICollection<Appointment> Appointments { get; set; }
+        public ICollection<PatientNotifications> Notifications { get; set; }
     }
 }
-
