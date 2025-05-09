d3ce4cae0b89b7af1a1e54d9dfbdd83124ee3e86
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-09 21:56:02 +0530

    add migration files

---
+++ PresCrypt-Backend/Migrations/20250427063313_UpdatedPatientNotifications.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PresCrypt_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedPatientNotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PatientNotifications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PatientId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientNotifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatientNotifications_Patient_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patient",
+                        principalColumn: "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientNotifications_PatientId",
+                table: "PatientNotifications",
+                column: "PatientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PatientNotifications");
+        }
+    }
+}
---
+++ PresCrypt-Backend/Migrations/20250430142013_AddTypeandDoctorIdToPatientNotifications.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PresCrypt_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTypeandDoctorIdToPatientNotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DoctorId",
+                table: "PatientNotifications",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "PatientNotifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientNotifications_DoctorId",
+                table: "PatientNotifications",
+                column: "DoctorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PatientNotifications_Doctor_DoctorId",
+                table: "PatientNotifications",
+                column: "DoctorId",
+                principalTable: "Doctor",
+                principalColumn: "DoctorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PatientNotifications_Doctor_DoctorId",
+                table: "PatientNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PatientNotifications_DoctorId",
+                table: "PatientNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "PatientNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "PatientNotifications");
+        }
+    }
+}
