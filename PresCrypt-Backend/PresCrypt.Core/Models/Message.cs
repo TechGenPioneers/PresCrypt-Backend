28a23b2812bf0d5e426aa259faade5f610146339
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-06-08 23:28:48 +0530

    Create Message model

---
+++ PresCrypt-Backend/PresCrypt.Core/Models/Message.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresCrypt_Backend.PresCrypt.Core.Models
+{
+    public class Message
+    {
+        [Key]
+        public string Id { get; set; } 
+
+        [Required]
+        public string SenderId { get; set; }  
+        [Required]
+        public string SenderType { get; set; } 
+
+        [Required]
+        public string ReceiverId { get; set; }
+        [Required]
+        public string ReceiverType { get; set; } 
+
+        public string Text { get; set; }
+        public byte[]? Image { get; set; }
+        public DateTime SendAt { get; set; } 
+        public Boolean IsReceived { get; set; } 
+        public Boolean IsRead { get; set; } 
+    }
+
+}
