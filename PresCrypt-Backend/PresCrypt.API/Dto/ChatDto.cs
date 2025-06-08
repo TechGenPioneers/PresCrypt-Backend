324f40d6d91e8fdecca2002959007a84fb467303
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-06-08 23:27:47 +0530

    create ChatDto and ChatUsersDto

---
+++ PresCrypt-Backend/PresCrypt.API/Dto/ChatDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PresCrypt_Backend.PresCrypt.API.Dto
+{
+    public class ChatDto
+    {
+        public string? Id { get; set; }
+        public string SenderId { get; set; }
+        public string? SenderType { get; set; }
+        public string ReceiverId { get; set; }
+        public string? ReceiverType { get; set; }
+        public string Text { get; set; }
+        public byte[]? Image { get; set; }
+        public DateTime? SendAt { get; set; }
+        public Boolean? IsReceived { get; set; }
+        public Boolean? IsRead { get; set; }
+    }
+}
---
+++ PresCrypt-Backend/PresCrypt.API/Dto/ChatUsersDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PresCrypt_Backend.PresCrypt.API.Dto
+{
+    public class ChatUsersDto
+    {
+        public string FullName { get; set; }
+        public string ReceiverId { get; set; }
+        public byte[] Image { get; set; }
+        public string LastMessage { get; set; }
+    }
+}
